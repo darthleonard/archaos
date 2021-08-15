1d956dd7cc01730356a4fac26e791e6ad8f021a1
darthleonard <leolinuxmx@gmail.com>
2021-08-15 14:55:05 -0500

    two components added

--- apps/archaos-ng/projects/sice/src/app/app-routing.module.ts
+++ apps/archaos-ng/projects/sice/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './projects/sice/shared/page-not-found/page-not-found.component';
+import { StudentsComponent } from './projects/sice/students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'students', component: StudentsComponent },
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes/*, { enableTracing: true }*/)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- apps/archaos-ng/projects/sice/src/app/app.module.ts
+++ apps/archaos-ng/projects/sice/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './projects/sice/shared/toolbar/toolbar.component';
 import { SideNavComponent } from './projects/sice/shared/side-nav/side-nav.component';
+import { StudentsComponent } from './projects/sice/students/students.component';
+import { PageNotFoundComponent } from './projects/sice/shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    SideNavComponent
+    SideNavComponent,
+    StudentsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
